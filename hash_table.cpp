de69f6f84abd0b4880fc7f3cda50894ee0a57def
AnastasMIPT <AtosBel@ya.ru>
2020-05-02 20:23:12 +0300

    Add CRC32 hash

--- hash_table.cpp
+++ hash_table.cpp
@@ -8,11 +8,47 @@
 
 const unsigned long long Max_val = 18446744073709551615U;
 
-typedef unsigned long long hash_t;
-typedef std::function<hash_t (const char*)> hash_func_t;
+typedef unsigned int hash_t;
+typedef std::function<hash_t (const unsigned char*, unsigned long)> hash_func_t;
+
+template<typename ValueType>
+class MapIterator : public std::iterator<std::forward_iterator_tag, ValueType, int> {
+
+    int num;
+    ValueType* data;
+    int* prev;
+    int* next;
+
+public:
+    MapIterator (int point, ValueType* data, int* next, int* prev) :
+            num (point), data (data), prev (prev), next (next) {}
+
+    MapIterator (const MapIterator& it) :
+            num (it.num), data (it.data), prev (it.prev), next (it.next) {}
+
+    bool operator== (MapIterator const& it) const { return num == it.num; }
+
+    bool operator!= (MapIterator const& it) const { return num != it.num; }
+
+    typename MapIterator::reference operator* () const { return data[num]; }
+
+    MapIterator& operator++ () {
+        num = next[num];
+        return *this;
+    }
+
+    MapIterator operator++ (int) {
+        MapIterator temp = *this;
+        num = next[num];
+        return temp;
+    }
+
+};
+
+
 
 class HashTable {
-    static hash_t default_hash (const char* string);
+    static hash_t default_hash (const unsigned char* string, unsigned long len);
     size_t size;
     hash_func_t hash;
 public:
@@ -55,21 +91,21 @@ int main () {
     fread (buf.get(), sizeof (char), FileSize, f_in);
     GetWords (words, buf.get ());
 
-    HashTable hash_table1 (1009, hash_const_one);
-    HashTable hash_table2 (1009, hash_len_of_word);
-    HashTable hash_table3 (1009, hash_sum_ascii_mod_len);
-    HashTable hash_table4 (1009, hash_cycle);
+     HashTable hash_table1 (1009);
+    // HashTable hash_table2 (1009, hash_len_of_word);
+    // HashTable hash_table3 (1009, hash_sum_ascii_mod_len);
+    // HashTable hash_table4 (1009, hash_cycle);
     //HashTable hash_table5 (1009, hash_const_one);
 
     hash_table1.LoadTambleFromVec (words);
-    hash_table2.LoadTambleFromVec (words);
-    hash_table3.LoadTambleFromVec (words);
-    hash_table4.LoadTambleFromVec (words);
+    // hash_table2.LoadTambleFromVec (words);
+    // hash_table3.LoadTambleFromVec (words);
+    // hash_table4.LoadTambleFromVec (words);
 
     hash_table1.SizesOfListsInTable (f_out);
-    hash_table2.SizesOfListsInTable (f_out);
-    hash_table3.SizesOfListsInTable (f_out);
-    hash_table4.SizesOfListsInTable (f_out);
+    // hash_table2.SizesOfListsInTable (f_out);
+    // hash_table3.SizesOfListsInTable (f_out);
+    // hash_table4.SizesOfListsInTable (f_out);
 
     printf ("Hello!\n");
     fclose (f_in);
@@ -106,7 +142,7 @@ void HashTable::LoadTambleFromVec (std::vector<char*> &vec) {
 }
 
 void HashTable::insert (const char* str) {
-    unsigned long long index = (hash (str)) % size;
+    unsigned long long index = (hash ((unsigned char*) str, strlen (str)) % size);
     for (auto c : table[index]) {
         if (strcmp (str, c) == 0) {
             return;
@@ -115,9 +151,9 @@ void HashTable::insert (const char* str) {
     table[index].PushBack (str);
 }
 
-hash_t HashTable::default_hash (const char* string) {
-    return 1;
-}
+// hash_t HashTable::default_hash (const char* string) {
+//     return 1;
+// }
 
 hash_t hash_const_one (const char* str) {
     return 1;
@@ -190,4 +226,63 @@ void cleaning_text (FILE* f_in, unsigned long long FileSize) {
     delete[] (buff);
     delete[] (new_buf);
     fclose (f_out);
+}
+
+#define mmix(h,k) { k *= m; k ^= k >> r; k *= m; h *= m; h ^= k; }
+template <typename T>
+unsigned int MurmurHash2A ( const T * key, unsigned int seed = 17892, int len = sizeof (T))
+{
+	const unsigned int m = 0x5bd1e995;
+	const int r = 24;
+	unsigned int l = len;
+
+	const unsigned char * data = (const unsigned char *)key;
+
+	unsigned int h = seed;
+	unsigned int k;
+
+	while(len >= 4)
+	{
+		k = *(unsigned int*)data;
+
+		mmix(h,k);
+
+		data += 4;
+		len -= 4;
+	}
+
+	unsigned int t = 0;
+
+	switch(len)
+	{
+	case 3: t ^= data[2] << 16;
+	case 2: t ^= data[1] << 8;
+	case 1: t ^= data[0];
+	};
+
+	mmix(h,t);
+	mmix(h,l);
+
+	h ^= h >> 13;
+	h *= m;
+	h ^= h >> 15;
+
+	return h;
+}
+
+hash_t HashTable::default_hash (const unsigned char* buf, unsigned long len)
+{
+	unsigned long crc_table[256];
+	unsigned long crc;
+	for (int i = 0; i < 256; i++)
+	{
+		crc = i;
+		for (int j = 0; j < 8; j++)
+			crc = crc & 1 ? (crc >> 1) ^ 0xEDB88320UL : crc >> 1;
+		crc_table[i] = crc;
+	};
+	crc = 0xFFFFFFFFUL;
+	while (len--)
+	crc = crc_table[(crc ^ *buf++) & 0xFF] ^ (crc >> 8);
+	return crc ^ 0xFFFFFFFFUL;
 }
